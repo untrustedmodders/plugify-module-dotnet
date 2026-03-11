30f738b0db773161edc208e0d10ea0c8e167732e
qubka <nikita.ushakov0000@outlook.com>
2026-03-11 00:30:36 +0000

    fix: remove Plugin instance

--- managed/Plugify/Plugin.cs
+++ managed/Plugify/Plugin.cs
@@ -2,13 +2,6 @@
 
 public class Plugin : IEquatable<Plugin>, IComparable<Plugin>
 {
-    public static Plugin? Instance { get; private set; }
-
-    protected Plugin()
-    {
-        Instance = this;
-    }
-    
     public long Id { get; set; } = -1;
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
